d52456f4e3679e5463a13b1e6bc61111cc50af28
smilv <owou@icloud.com>
2019-04-08 15:28:26 +0800

    NODE_ENV

--- config/env.js
+++ config/env.js
@@ -1,9 +1,3 @@
 const apiPath = require("../api");
 
-module.exports = Object.assign(
-    {},
-    {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-    },
-    apiPath[process.env.BUILD_TYPE]
-);
+module.exports = Object.assign({}, apiPath[process.env.BUILD_TYPE]);
